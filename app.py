3f943bba6335fbc11d4d75837cc90d7b7d531756
kalyanchakravarthych <kalyanchakravarthych@gmail.com>
2022-06-25 11:29:52 +0530

    ML Project Updated

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask
+from housing.logger import logging
+from housing.exception import HousingException
+import sys
 
 app = Flask(__name__)
 
 @app.route("/", methods = ["GET", "POST"])
 def index():
+    logging.info("We are testing logging module")
     return "Starting Machine Learning Project"
 
 if __name__== "__main__":
